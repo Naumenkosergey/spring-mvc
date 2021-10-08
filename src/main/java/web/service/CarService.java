c9dcf28c8624d6d42b4ed3b516257d03a8942ca3
naymc <naumenkosergeyns@gmail.com>
2021-10-09 01:16:02 +0300

    CarController+Service

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,15 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService {
+
+    public static List<Car> getCarsCount(List<Car> cars, int count) {
+        if (count >= 5)
+            return cars;
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
